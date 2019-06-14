78581760cc223ef3c7f5a0a8bc5c4e9dfd9a2d10
Miguel Martinez <miguelitalia@hotmail.com>
2019-06-14 17:35:12 +0100

    added boilerplate flask to app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    return render_template()
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+#set FLASK_APP=app.py
+#set FLASK_ENV=development

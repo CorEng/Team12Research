096473e4028de3ae25c3bae15cc7b487a3f06b29
Miguel Martinez <miguelitalia@hotmail.com>
2019-06-16 11:04:12 +0100

    put code in get_data.py inside a function and added comments to it

--- get_data.py
+++ get_data.py
@@ -1,28 +1,31 @@
 import pandas as pd
 
-df_stops = pd.ExcelFile("dublinbusstops.xls")
-df = df_stops.parse("Sheet1")
+def get_data():
+    """Use dublin bus stop data file to obtain the necessary data and save it on a different smaller file"""
 
-df["Latitude"] = None
-df["Longitude"] = None
+    # Open file
+    df_stops = pd.ExcelFile("dublinbusstops.xls")
+    df = df_stops.parse("Sheet1")
 
+    # Create new columns
+    df["Latitude"] = None
+    df["Longitude"] = None
 
+    # Take the lat and lon and put them in the correct columns - only to str types
+    for i, value in enumerate(df["Coordinates"]):
 
+        if type(df["Coordinates"][i]) != float:
+            df.at[i, "Latitude"] = df["Coordinates"][i][7:13]
+            df.at[i, "Longitude"] = df["Coordinates"][i][15:]
+        else:
+            continue
 
-for i, value in enumerate(df["Coordinates"]):
+    # Drop unwanted data
+    df = df.dropna()
+    df = df.drop(["Unique British Isles Id", "Map", "Coordinates", "Projection", "Easting", "Prov", "Unnamed: 9",
+                  "Northing"], axis = 1)
 
-    if type(df["Coordinates"][i]) != float:
-        df.at[i, "Latitude"] = df["Coordinates"][i][7:13]
-        df.at[i, "Longitude"] = df["Coordinates"][i][15:]
-    else:
-        continue
-
-df = df.dropna()
-df = df.drop(["Unique British Isles Id", "Map", "Coordinates", "Projection", "Easting", "Prov", "Unnamed: 9",
-              "Northing"], axis = 1)
-
-print(df.tail())
-
-with pd.ExcelWriter('final_stops.xls') as writer:
-    df.to_excel(writer)
+    # Create and write new file
+    with pd.ExcelWriter('final_stops.xls') as writer:
+        df.to_excel(writer)
 

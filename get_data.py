45b4efe853a99e6afd9fe4a78ba6df8092065404
Miguel Martinez <miguelitalia@hotmail.com>
2019-06-17 17:20:13 +0100

    added dummy index.html, map.js, style.css in correct flask directories and correct dataset for bus stops

--- get_data.py
+++ get_data.py
@@ -1,16 +1,22 @@
 import csv
 
-def get():
+def get_stops():
 
     with open('stops.csv', encoding="utf8") as read_file:
         read_csv = csv.reader(read_file, delimiter=",")
 
         stops = []
+        count = 0
         for i, row in enumerate(read_csv):
-            stops.append([row[0], row[2], row[4]])
+            if i == 0:
+                continue
+            else:
+                if not float(row[0]) or not float(row[2]):
+                    print(count)
+                else:
+                    stops.append([float(row[0]), float(row[2]), row[4]])
+            count += 1
 
         read_file.close()
 
     return stops
-
-print(get())

f1af0ff3eae566c3d932cbc88075ab23d677e4d4
Miguel Martinez <miguelitalia@hotmail.com>
2019-06-20 17:04:51 +0100

    added list of stops to show up in stops form fields, added getting the stops from form to then get the stops needed from each route on the stops

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from get_data import *
 
 app = Flask(__name__)
@@ -9,6 +9,14 @@ def index():
     lat_lon_stops = a.get_stops()
     return render_template('index.html', lat_lon_stops=lat_lon_stops)
 
+@app.route('/handle_search', methods = ["POST"])
+def get_search():
+    stopA = request.form.get("stopA")
+    stopB = request.form.get("stopB")
+
+    a = Stops()
+    print(a.a_to_b(stopA, stopB))
+    return redirect('/')
 
 
 if __name__ == '__main__':

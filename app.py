9bcf223a24ce12276cd99a670aa62c082142d45e
Jia Ru <jiaru.he@ucdconnect.ie>
2019-06-27 12:30:26 +0100

    separate folder and change css

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
+from get_data import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    lat_lon_stops = []
+    num_name = Stops().get_all_stops()
+    return render_template('index.html', lat_lon_stops=lat_lon_stops, num_name=num_name)
 
-    return render_template()
+@app.route('/handle_search', methods = ["POST"])
+def handle_search():
+    stopA = request.form.get("stopA")
+    stopB = request.form.get("stopB")
+    route = request.form.get("route").upper()
+    scroll = "map"
 
+    a = Stops()
+    b = a.a_to_b(stopA, stopB, route)
+    lat_lon_stops = a.get_searched_stops(b)
+    return render_template('index.html', lat_lon_stops=lat_lon_stops, scroll=scroll)
 
 
 if __name__ == '__main__':

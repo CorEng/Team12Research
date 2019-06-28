a982067643d13cab5e2ddfd317cf772ff64e9c72
Miguel Martinez <miguelitalia@hotmail.com>
2019-06-28 11:02:02 +0100

    added option of bus public transport for maps directions and amended index.html

--- app.py
+++ app.py
@@ -11,15 +11,8 @@ def index():
 
 @app.route('/handle_search', methods = ["POST"])
 def handle_search():
-    stopA = request.form.get("stopA")
-    stopB = request.form.get("stopB")
-    route = request.form.get("route").upper()
-    scroll = "map"
 
-    a = Stops()
-    b = a.a_to_b(stopA, stopB, route)
-    lat_lon_stops = a.get_searched_stops(b)
-    return render_template('index.html', lat_lon_stops=lat_lon_stops, scroll=scroll)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
--- static/js/map.js
+++ static/js/map.js
@@ -11,32 +11,32 @@ function initMap() {
     var map = new google.maps.Map(
         document.getElementById('map'), {zoom: 14, center: ireland});
 
-        var infowindow = new google.maps.InfoWindow();
+    var infowindow = new google.maps.InfoWindow();
 
-        // 1/3 to center map around the markers
-        var bounds = new google.maps.LatLngBounds();
+    // 1/3 to center map around the markers
+    var bounds = new google.maps.LatLngBounds();
 
-        // place different stations markers on the map
-        for (var i = 0; i < latLonStops.length; i++) {
-            var location = {lat: latLonStops[i][0], lng: latLonStops[i][1]};
-            // 2/3 center map around the markers
-            bounds.extend(location);
-            var marker = new google.maps.Marker({animation: google.maps.Animation.DROP, position: location, map: map});
+    // place different stations markers on the map
+    for (var i = 0; i < latLonStops.length; i++) {
+        var location = {lat: latLonStops[i][0], lng: latLonStops[i][1]};
+        // 2/3 center map around the markers
+        bounds.extend(location);
+        var marker = new google.maps.Marker({animation: google.maps.Animation.DROP, position: location, map: map});
 
-            google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                return function() {
-                    infowindow.setContent('<div>' +
-                                                '<p class="detail">' + latLonStops[i][3] + '</p>' +
-                                                '<p class="detail">' + latLonStops[i][2] + '</p>' +
-                                                '<p class="detail">' + latLonStops[i][4] + '</p>' +
-                                        '</div>');
-                    infowindow.open(map, marker);
-                }
-            })(marker, i));
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent('<div>' +
+                                            '<p class="detail">' + latLonStops[i][3] + '</p>' +
+                                            '<p class="detail">' + latLonStops[i][2] + '</p>' +
+                                            '<p class="detail">' + latLonStops[i][4] + '</p>' +
+                                    '</div>');
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
 
 //             3/3 center map around the markers
 //            map.fitBounds(bounds);
-         }
+     }
 
     if (latLonStops.length < 1) {
     // start the user's geolocations-------------------------------------------
@@ -66,6 +66,8 @@ function initMap() {
         }
     };// initMap()
 
+
+
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
@@ -81,7 +83,11 @@ function calcRoute() {
   var request = {
     origin: start,
     destination: end,
-    travelMode: 'DRIVING'
+    travelMode: 'TRANSIT',
+    TransitOptions: {
+        modes: ['BUS'],
+        routingPreference: 'LESS_WALKING'
+    }
   };
   directionsService.route(request, function(result, status) {
     if (status == 'OK') {

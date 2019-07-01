28a508f5187c4caeac6fc9436e035e9c98dac0d7
Miguel Martinez <miguelitalia@hotmail.com>
2019-07-01 12:05:27 +0100

    added google maps directions to the google maps view in index.html

--- static/js/map.js
+++ static/js/map.js
@@ -1,72 +1,41 @@
+// The location of Ireland--------------------------------------------
+var dublin = {lat: 53.349605, lng:-6.264175 };
+var posA = {};
+var posB = {};
+
+
 // Initialize and add the map
 function initMap() {
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
 
-    var directionsService = new google.maps.DirectionsService();
-    var directionsDisplay = new google.maps.DirectionsRenderer();
-
-    // The location of Ireland--------------------------------------------
-    var ireland = {lat: 53.2734, lng: -7.77832031};
-
-    // The map, centered at Ireland
+    // The map, centered at Dublin
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 14, center: ireland});
+        document.getElementById('map'), {zoom: 14, center: dublin});
 
-    var infowindow = new google.maps.InfoWindow();
-
-    // 1/3 to center map around the markers
-    var bounds = new google.maps.LatLngBounds();
-
-    // place different stations markers on the map
-    for (var i = 0; i < latLonStops.length; i++) {
-        var location = {lat: latLonStops[i][0], lng: latLonStops[i][1]};
-        // 2/3 center map around the markers
-        bounds.extend(location);
-        var marker = new google.maps.Marker({animation: google.maps.Animation.DROP, position: location, map: map});
-
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infowindow.setContent('<div>' +
-                                            '<p class="detail">' + latLonStops[i][3] + '</p>' +
-                                            '<p class="detail">' + latLonStops[i][2] + '</p>' +
-                                            '<p class="detail">' + latLonStops[i][4] + '</p>' +
-                                    '</div>');
-                infowindow.open(map, marker);
-            }
-        })(marker, i));
-
-//             3/3 center map around the markers
-//            map.fitBounds(bounds);
-     }
-
-    if (latLonStops.length < 1) {
     // start the user's geolocations-------------------------------------------
     infoWindow = new google.maps.InfoWindow;
 
     // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-            };
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('You are here');
-            infoWindow.open(map);
-            map.setCenter(pos);
-        }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-        });
-        } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-            }
-        } else if (latLonStops.length > 0) {
-//             3/3 center map around the markers
-            map.fitBounds(bounds);
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('You are here');
+        infoWindow.open(map);
+        map.setCenter(pos);
+    }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+    });
+    } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
         }
-
-        directionsDisplay.setMap(map);
-
+    directionsDisplay.setMap(map);
     };// initMap()
 
 
@@ -81,20 +50,63 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
 
 function calcRoute() {
-  var start = document.getElementById('start').value;
-  var end = document.getElementById('end').value;
-  var request = {
-    origin: start,
-    destination: end,
-    travelMode: 'TRANSIT',
-    TransitOptions: {
-        modes: ['BUS'],
-        routingPreference: 'LESS_WALKING'
+
+    var start = posA;
+    var end = posB;
+    console.log(start);
+    console.log(end);
+
+    var request = {
+        origin: start,
+        destination: end,
+        travelMode: 'TRANSIT'
+//        TransitOptions: {
+//            modes: ['BUS'],
+//            routingPreference: 'LESS_WALKING'
+//        }
+      };
+
+    directionsService.route(request, function(result, status) {
+        if (status == 'OK') {
+          directionsDisplay.setDirections(result);
+        }
+      });
     }
-  };
-  directionsService.route(request, function(result, status) {
-    if (status == 'OK') {
-      directionsDisplay.setDirections(result);
+
+function stopA(){
+
+    // AutoComplete------------------------------------------------------------
+    var input = document.getElementById('start');
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      var place = autocomplete.getPlace();
+      var lat = place.geometry.location.lat();
+      var lng = place.geometry.location.lng();
+      posA = {
+      	lat: lat,
+      	lng:lng
+      	};
+        });
     }
-  });
-}
+
+function stopB(){
+
+    // AutoComplete------------------------------------------------------------
+    var input = document.getElementById('end');
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      var place = autocomplete.getPlace();
+      var lat = place.geometry.location.lat();
+      var lng = place.geometry.location.lng();
+      posB = {
+      	lat: lat,
+      	lng:lng
+      	};
+        });
+    }
+
+google.maps.event.addDomListener(window, 'load', calcRoute);

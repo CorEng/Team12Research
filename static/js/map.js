c9a9099c57d81ad67e5d826f8d893fec24274291
Jia Ru <jiaru.he@ucdconnect.ie>
2019-07-02 15:52:46 +0100

    adjust map position and size, adjust different section structure

--- static/js/map.js
+++ static/js/map.js
@@ -1,71 +1,136 @@
+var dublin = {lat: 53.349605, lng:-6.264175 };
+var posA = {};
+var posB = {};
+
 // Initialize and add the map
 function initMap() {
-    // The location of Ireland--------------------------------------------
-    var ireland = {lat: 53.2734, lng: -7.77832031};
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
 
-    // The map, centered at Ireland
+    // The map, centered at Dublin
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 14, center: ireland});
+        document.getElementById('map'), {zoom: 12, center: dublin});
 
-        var infowindow = new google.maps.InfoWindow();
-
-        // 1/3 to center map around the markers
-        var bounds = new google.maps.LatLngBounds();
-
-        // place different stations markers on the map
-        for (var i = 0; i < latLonStops.length; i++) {
-            var location = {lat: latLonStops[i][0], lng: latLonStops[i][1]};
-            // 2/3 center map around the markers
-            bounds.extend(location);
-            var marker = new google.maps.Marker({animation: google.maps.Animation.DROP, position: location, map: map});
-
-            google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                return function() {
-                    infowindow.setContent('<div>' +
-                                                '<p class="detail">' + latLonStops[i][3] + '</p>' +
-                                                '<p class="detail">' + latLonStops[i][2] + '</p>' +
-                                                '<p class="detail">' + latLonStops[i][4] + '</p>' +
-                                        '</div>');
-                    infowindow.open(map, marker);
-                }
-            })(marker, i));
-
-//             3/3 center map around the markers
-//            map.fitBounds(bounds);
-         }
-
-    if (latLonStops.length < 1) {
     // start the user's geolocations-------------------------------------------
     infoWindow = new google.maps.InfoWindow;
 
     // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-            };
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('You are here');
-            infoWindow.open(map);
-            map.setCenter(pos);
-        }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-        });
-        } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-            }
-        } else if (latLonStops.length > 0) {
-//             3/3 center map around the markers
-            map.fitBounds(bounds);
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('You are here');
+        infoWindow.open(map);
+        map.setCenter(pos);
+    }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+    });
+    } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
         }
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('left-panel'));
     };// initMap()
 
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                      'Error: The Geolocation service failed.' :
+                      'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+    }
+
+// Google Directions
+function calcRoute() {
+    var start = posA;
+    var end = posB;
+
+    var request = {
+        origin: start,
+        destination: end,
+        travelMode: 'TRANSIT',
+          transitOptions: {
+            modes: ['BUS'],
+            routingPreference: 'FEWER_TRANSFERS'
+          },
+      };
+
+    directionsService.route(request, function(result, status) {
+        if (status == 'OK') {
+          directionsDisplay.setDirections(result);
+        } else {
+          window.alert('Directions request failed due to ' + status);
         }
+      });
+    }
+
+// Take input from origin of trip
+function stopA(){
+    // AutoComplete------------------------------------------------------------
+    var input = document.getElementById('start');
+
+    var dublinBounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(53.1235, -6.5021),
+      new google.maps.LatLng(53.4872, -6.0209));
+
+    var defaultBounds = {
+        bounds: dublinBounds,
+        strictBounds: true
+        };
+
+    var autocomplete = new google.maps.places.Autocomplete(input, defaultBounds);
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      var place = autocomplete.getPlace();
+      var lat = place.geometry.location.lat();
+      var lng = place.geometry.location.lng();
+      posA = {
+      	lat: lat,
+      	lng: lng
+      	};
+        });
+    }
+
+// Take input from Destination of trip
+function stopB(){
+    // AutoComplete------------------------------------------------------------
+    var input = document.getElementById('end');
+
+    var dublinBounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(53.1235, -6.5021),
+      new google.maps.LatLng(53.4872, -6.0209));
+
+    var defaultBounds = {
+        bounds: dublinBounds,
+        strictBounds: true
+        };
+
+    var autocomplete = new google.maps.places.Autocomplete(input, defaultBounds);
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      var place = autocomplete.getPlace();
+      var lat = place.geometry.location.lat();
+      var lng = place.geometry.location.lng();
+      posB = {
+      	lat: lat,
+      	lng: lng
+      	};
+        });
+    }
+
+function ajax() {
+    calcRoute();
+    $.getJSON($SCRIPT_ROOT + '/directions', {
+        postA: document.getElementById("start").value,
+        postB: document.getElementById("end").value
+    }, function(data) {
+        var response = data;
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', calcRoute);

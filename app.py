d42ccfb646770e17ad86306a5329ede09200f7e1
Jia Ru <jiaru.he@ucdconnect.ie>
2019-07-22 11:55:49 +0100

    UI test and output part design

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, jsonify
+from datetime import datetime, timedelta
 from get_data import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    todayObj = datetime.datetime.now()
+    today = todayObj.strftime("%Y-%m-%d")
+    max = todayObj + timedelta(days=90)
+    maxDate = max.strftime("%Y-%m-%d")
+
+    return render_template('index.html', today=today, maxDate=maxDate)
 
 @app.route("/directions", methods=['GET', 'POST'])
 def directions():
     postA = request.args.get('postA')
     postB = request.args.get('postB')
+    frontTime = request.args.get('htmlTime')
+    frontDate = request.args.get('htmlDate')
 
-    # print(Stops().get_direct_goo(postA, postB))
-    return "HELLO"
 
+    intermediate = Stops()
+    secs = intermediate.getSeconds(frontDate, frontTime)
+    get_goo_data = intermediate.get_direct_goo(postA, postB, secs)
+    interStops = intermediate.fin(get_goo_data)
+    jsonObj = get_goo_data
+    full = {"interstops": interStops, "gooData": jsonObj}
 
+    return jsonify(full)
 
 if __name__ == '__main__':
     app.run(debug=True)

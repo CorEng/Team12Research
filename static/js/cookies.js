3301117ea91c21686bef4755bd9812fc8c63b633
Miguel Martinez <miguelitalia@hotmail.com>
2019-08-10 20:25:05 +0100

    cookie number to add +1 functionality added, working on saving the neccessary lists to the cookies

--- static/js/cookies.js
+++ static/js/cookies.js
@@ -1,18 +1,46 @@
 
 function setCookies() {
-    console.log(document.cookie);
 
-    var cookiesLength = document.cookie.length;
-    console.log(cookiesLength);
-    var variableList = [googleData, intermediateStops, backAmenities]
-
-    if (cookiesLength == 5) {
-        alert("Apologies you have already 5 saved searches saved")
+    if (document.cookie.indexOf(";") == -1) {
+        if (document.cookie.length < 1) {
+            cookiesLength = 0;
+        }
+        else if (document.cookie.length > 0) {
+            cookiesLength = 1;
+        }
     }
     else {
-        var dataCookies = document.cookie = "cookie" + (cookiesLength -1).toString() + "=" + JSON.stringify
-        (variableList);
-    }
+        cookiesLength = document.cookie.split(" ").length;
+    };
+
+
+
+
+    console.log(document.cookie);
+    console.log(cookiesLength);
+
+    var variableList = [JSON.stringify(googleData), JSON.stringify(intermediateStops), JSON.stringify(backAmenities)];
+
+    x = document.cookie = "cookie" + (cookiesLength).toString() + "=" + JSON.stringify(variableList);
+
+
+
+
+//    if (cookiesLength == 5) {
+//        alert("Apologies you have reached the max of 5 saved searches");
+//    }
+//    else {
+//        if (variableList[0] == null) {
+//            alert("No previous search available to save, please perform a search to save it");
+//        }
+//        else {
+//
+//            x = document.cookie = "cookie" + (cookiesLength).toString() + "=" + JSON.stringify(variableList);
+//            console.log("after trying to save");
+//            console.log(document.cookie);
+//        }
+//    }
+
 
 
 

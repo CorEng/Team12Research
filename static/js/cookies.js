8be88b7300992931fbd074868720a0f6a838880e
Miguel Martinez <miguelitalia@hotmail.com>
2019-08-12 17:33:29 +0100

    added add to favourites to every search and remove item from favourites button to every item in favourites, minor code flow error fixes with favourites

--- static/js/cookies.js
+++ static/js/cookies.js
@@ -18,6 +18,7 @@ function setCookies() {
             var variableList = [googleData["routes"][0]["legs"][0]["start_address"], googleData["routes"][0]["legs"][0]["end_address"]];
             localStorage.setItem("cookie" + (count).toString(), JSON.stringify(variableList));
             count++
+            alert("This search has been ADDED to your Favourites")
         }
         else {
             alert("No previous search available to save, please perform a search to save it");
@@ -41,6 +42,15 @@ function getCookie(num) {
 
 function removeCookie(num) {
     localStorage.removeItem("cookie" + num.toString());
+    showFavs();
+    setTimeout(function() {
+            if (getCount > 0) {
+                $("div.favourites").slideDown("slow");
+            }
+            else {
+                alert("Favourites list is empty");
+            }},
+            950);
 }
 
 function clearAll() {
@@ -56,7 +66,6 @@ function showFavs() {
         if (localStorage.getItem("cookie" + i.toString())) {
 
             var favData = JSON.parse(localStorage.getItem("cookie" + i.toString()));
-            console.log(favData);
 
             var favButt = document.createElement("div");
             favButt.setAttribute("class", "favButt");
@@ -78,11 +87,40 @@ function showFavs() {
             favDest.appendChild(dest);
             favButt.appendChild(favDest);
 
+            var favRem = document.createElement("div");
+            favRem.setAttribute("class", "favRem");
+            var rem = document.createElement("p");
+            rem.setAttribute("onclick", "removeCookie(" + i.toString() + ")")
+            var remText = document.createTextNode("Remove");
+            rem.appendChild(remText);
+            favRem.appendChild(rem);
+            favButt.appendChild(favRem);
+
 
             document.getElementById("favs").appendChild(favButt);
-
-
         }
+    };
+    var elem = document.getElementById("ops");
+    var pixelsElem = window.getComputedStyle(elem, null).getPropertyValue("height");
+    if (pixelsElem == "auto") {
+        var pixels = 0;
     }
+    else {
+        var pixels = parseInt(pixelsElem.slice(0, -2))
+    };
+    window.scrollTo(0, 1125 + pixels);
+
+    if ($("div.favourites").css("display") != "none") {
+        $("div.favourites").slideUp("slow");
+    }
+    else if ($("div.favourites").css("display") == "none") {
+        if (getCount() == 0) {
+            alert("No Search in Favourites to Show. Please Make a Search to Then Save It")
+            window.scrollTo(0,0);
+        }
+        else {
+            $("div.favourites").slideDown("slow");
+        }
+    };
 }
 
